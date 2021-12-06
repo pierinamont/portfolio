a814edc6fa1fd7f90b0f9fab94e24d570f1c2252
Pierina Montalva <pierinamfatur@gmail.com>
2021-12-06 10:32:01 -0500

    componente para home

---
+++ my-portfolio/src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col items-center">
+    <img class="w-60" src="../assets/laptop.svg" alt="">
+    <h3 class="text-4xl font-bold text pb-3.5">Hola! Soy Pierina</h3>
+    <p class="text-xl italic pb-3.5">Front-end Developer Junior</p>
+    <span class="flex">
+      <a href="https://github.com/pierinamont" target="_blank" >
+        <img class="pr-6 text-blue cursor-pointer" src="../assets/github.svg" alt="github icon">
+      </a>
+
+      <a href="https://www.linkedin.com/in/pierina-montalva-fatur/" target="_blank">
+        <img class="cursor-pointer" src="../assets/linkedin.svg" alt="linkdIn icon">
+      </a>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+};
+</script>
---
+++ my-portfolio/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      blue: '#2E5BFF',
+      lightBlue: 'rgba(46, 91, 255, 0.15)',
+      text: '#303030',
+      shadow: '0px 18px 17px rgba(46, 91, 255, 0.15)',
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
