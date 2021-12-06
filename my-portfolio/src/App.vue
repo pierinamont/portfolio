a9baddfb7165d15f69aa8ba27b7bc773ae1966aa
Pierina Montalva <pierinamfatur@gmail.com>
2021-12-06 11:55:03 -0500

    componente about

--- my-portfolio/src/App.vue
+++ my-portfolio/src/App.vue
@@ -1,9 +1,9 @@
 <template>
  <div>
-    <div id="nav" class="flex justify-end">
+    <div id="nav" class="flex justify-end pt-8">
       <router-link to="/" class="mr-8">Sobre mí</router-link>
-      <router-link to="/about" class="mr-8">Proyectos</router-link>
-      <router-link to="/about" class="mr-4 lg:mr-8">
+      <router-link to="/" class="mr-8">Proyectos</router-link>
+      <router-link to="/" class="mr-4 lg:mr-8">
         <span class="p-3 bg-blue hover:bg-blueDark">Contáctame</span>
       </router-link>
     </div>
--- my-portfolio/src/views/Home.vue
+++ my-portfolio/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
-    <Hero/>
+    <Hero class="mt-20"/>
+    <About class="mt-56"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Hero from '@/components/Hero.vue';
+import About from '../components/About.vue';
 
 export default {
   name: 'Home',
   components: {
     Hero,
+    About,
   },
 };
 </script>
